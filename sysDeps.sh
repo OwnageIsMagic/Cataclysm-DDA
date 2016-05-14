23486aac97c58887ae7744a4220eb47674357e3a
OwnageIsMagic <OwnageIsMagic@users.noreply.github.com>
2016-05-14 22:49:04 +0300

    asd

--- sysDeps.sh
+++ sysDeps.sh
@@ -3,7 +3,4 @@ set -ev
 #sudo apt-get install openjdk-7-jdk
 
 # Get Android NDK
-if [ `uname -m` = x86_64]; then wget http://dl.google.com/android/ndk/android-ndk-r10d-linux-x86_64.bin -O ndk.bin; else wget http://dl.google.com/android/ndk/android-ndk-r10d-linux-x86.bin -O ndk.bin; fi
-chmod a+x ndk.bin
-#- ./ndk.bin -y | grep -v Extracting
-./ndk.bin 1> /dev/null 2>&1
+if [[ ! -e android-ndk-root/ndk-build ]]; then wget dl.google.com/android/repository/android-ndk-r11c-linux-x86_64.zip -O ndk.zip && unzip ndk.zip && mv android-ndk-r11c android-ndk-root; fi
