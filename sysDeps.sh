0eb1ddbe97ce5ed89aaa7c2034779fd8840d1abc
OwnageIsMagic <OwnageIsMagic@users.noreply.github.com>
2016-05-15 00:05:13 +0300

    asdas

--- sysDeps.sh
+++ sysDeps.sh
@@ -7,7 +7,7 @@ set -ev
 
 #	Get Android NDK
 if [[ ! -e ~/android-ndk-root/ndk-build ]]; then
-	wget dl.google.com/android/repository/android-ndk-r11c-linux-x86_64.zip -O ~/ndk.zip
-	unzip ~/ndk.zip -d ~ 1>/dev/null
+	wget dl.google.com/android/repository/android-ndk-r11c-linux-x86_64.zip -O ~/ndk.zip &&
+	unzip ~/ndk.zip -d ~ 1>/dev/null &&
 	mv ~/android-ndk-r11c ~/android-ndk-root;
 fi
