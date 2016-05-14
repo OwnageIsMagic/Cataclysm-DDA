e158c3a24e82c366cb7199a247f853b752e0439f
OwnageIsMagic <OwnageIsMagic@users.noreply.github.com>
2016-05-14 23:18:23 +0300

    a

--- sysDeps.sh
+++ sysDeps.sh
@@ -1,4 +1,13 @@
 set -ev
 
+#	apt-get
+#sudo apt-get update 1>/dev/null 2>&1
 #sudo apt-get install openjdk-7-jdk
-sudo apt-get install zipalign
+#sudo apt-get install zipalign
+
+#	Get Android NDK
+if [[ ! -e ~/android-ndk-root/ndk-build ]]; then
+	wget dl.google.com/android/repository/android-ndk-r11c-linux-x86_64.zip -O ~/ndk.zip
+	unzip ~/ndk.zip
+	mv ~/android-ndk-r11c ~/android-ndk-root;
+fi
